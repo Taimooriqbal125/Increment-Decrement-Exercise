4a36ae8ecda60ca5f3c8f55c7d98b8d291af0e5e
Taimooriqbal125 <taimooriqbal125@gmail.com>
2024-04-30 19:58:29 +0500

    Exercise

---
+++ script.js
@@ -0,0 +1,18 @@
+// Increment Decrment Exercise
+
+
+// Q 1
+var a =  2;
+var b = ++a * 2 ;
+console.log( "RESULT" , b) ;
+
+// Q 2
+var x = 5;
+var y = x-- + 2  ;
+console.log( "RESULT" ,y) ;
+
+// Q 3
+var x = 3 ;
+var y = ++x + x++ + ++x ; 
+console.log( "RESULT" ,y);
+
